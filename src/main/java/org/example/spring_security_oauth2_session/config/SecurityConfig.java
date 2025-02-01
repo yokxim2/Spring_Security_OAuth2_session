51b22fcd936c657f52d35cca8e3a95c55c46103d
yokxim <yooksi53@gmail.com>
2025-02-01 23:01:00 +0900

    feat[security-config] 시큐리티 설정 파일 생성
    - csrf, formlogin, basicHttp 모두 비활성화
    - oauth2Login 활성화 (기본값)

---
+++ src/main/java/org/example/spring_security_oauth2_session/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package org.example.spring_security_oauth2_session.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf((csrf) -> csrf.disable());
+
+        http
+                .formLogin((login) -> login.disable());
+
+        http
+                .httpBasic((basic) -> basic.disable());
+
+        http
+                .oauth2Login(Customizer.withDefaults());
+
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/oauth2/**", "/login/**").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
