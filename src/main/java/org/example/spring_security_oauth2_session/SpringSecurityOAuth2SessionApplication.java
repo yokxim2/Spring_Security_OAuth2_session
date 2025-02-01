3b84eff16baf6445ad77ac8f55e4f934e2467de4
yokxim <yooksi53@gmail.com>
2025-02-01 21:41:26 +0900

    Initial commit

---
+++ src/main/java/org/example/spring_security_oauth2_session/SpringSecurityOAuth2SessionApplication.java
@@ -0,0 +1,13 @@
+package org.example.spring_security_oauth2_session;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuth2SessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOAuth2SessionApplication.class, args);
+    }
+
+}
